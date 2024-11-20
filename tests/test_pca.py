98ebe5533bb28f377ff28ad1455bd687791f9b14
Alice Le Brigant <alice.le-brigant@univ-paris1.fr>
2024-11-20 15:15:51 +0100

    rename test folder to tests

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -1,9 +1,9 @@
 from geomstats.geometry.special_orthogonal import SpecialOrthogonal
 from geomstats.geometry.symmetric_matrices import SymmetricMatrices
 from numpy.linalg import norm
-from pca.bures_wasserstein_principal_geodesic_analysis import BuresWassersteinPGA, BuresWassersteinPGAND
-from pca.bures_wasserstein_tangent_pca import BuresWassersteinTPCA
-from tools.compute import make_rotation_2d
+from pca.bures_wasserstein_principal_geodesic_analysis import BuresWassersteinPGA, BuresWassersteinPGAND, BuresWassersteinTPCA
+#from pca.bures_wasserstein_tangent_pca import BuresWassersteinTPCA
+from tools.compute import make_rotation_2d, project
 from tools.generate import *
 
 
@@ -20,6 +20,46 @@ def check_unit_norm(vec, tol=TOL):
 
 def check_orthogonality(vec_1, vec_2, tol=TOL):
     assert np.abs(np.sum(vec_1 * vec_2)) < tol
+
+
+def test__bures_wasserstein_pga__init_dim2(n_points=10, tol=1e-6, seed=123):
+    """Check initialization of each component in dimension 2."""
+    spd_space = SPDMatrices(2)
+    np.random.seed(seed) if seed is not None else np.random.seed(np.random.randint(100))
+    points_spd = spd_space.random_point(n_points)
+    sq_roots = np.stack([sqrtm(pt_spd) for pt_spd in points_spd])
+    pga = BuresWassersteinPGA(2)
+
+    points, angles, mean_1, vec_1 = pga.component_1_init(sq_roots)
+    assert np.all(np.abs(project(points) - points_spd) < tol)
+    check_horizontality(vec_1, mean_1, tol)
+    check_unit_norm(vec_1, tol)
+
+    points_2, mean_2, vec_2, angle_1, time = pga.component_2_init(angles, mean_1, vec_1, sq_roots)
+    rotation_1 = make_rotation_2d(angle_1)
+    assert np.all(np.abs(mean_2 - mean_1) < tol)
+    assert np.all(np.abs(points_2 - points) < tol)
+    check_horizontality(vec_2, mean_2, tol)
+    check_horizontality(vec_1 @ rotation_1, mean_2, tol)
+    check_orthogonality(vec_2, vec_1 @ rotation_1, tol)
+    check_unit_norm(vec_2, tol)
+
+    angle_1, time = np.random.rand(2)
+    rotation_1 = make_rotation_2d(angle_1)
+    mean_2 = (mean_1 + time * vec_1) @ rotation_1
+    vec_aux = np.random.rand(2, 2)
+    vec_2 = (vec_aux + vec_aux.T) @ mean_2
+    vec_1_mean_2 = vec_1 @ rotation_1
+    vec_2 = vec_2 - np.sum(vec_2 * vec_1_mean_2) * vec_1_mean_2
+    vec_2 /= norm(vec_2)
+    points_3, mean_3, vec_3, angle_2 = pga.component_3_init(angles, mean_2, vec_1, vec_2, angle_1, sq_roots)
+    rotation_2 = make_rotation_2d(angle_2)
+    assert np.all(np.abs(mean_3 - mean_2) < tol)
+    assert np.all(np.abs(points_3 - points) < tol)
+    check_horizontality(vec_3, mean_3, tol)
+    check_orthogonality(vec_3, vec_1 @ rotation_1 @ rotation_2, tol)
+    check_orthogonality(vec_3, vec_2 @ rotation_2, tol)
+    check_unit_norm(vec_3, tol)
 
 
 def test__bures_wasserstein_pga__step1(dim=3, n_points=10, seed=123):
@@ -47,14 +87,14 @@ def test__bures_wasserstein_pga__step2_dim2(n_points=40):
     mean = np.random.rand(dim, dim)
     vec_1 = sym_space.random_point() @ mean
     pga = BuresWassersteinPGA(dim)
-    new_mean, new_vec = pga.component_1_step_2(points, mean, vec_1)
+    new_mean, new_vec = pga.component_1_step_2(mean, vec_1, points)
     check_unit_norm(new_vec)
     check_horizontality(new_vec, new_mean)
 
     vec_2 = make_rotation_2d(np.pi / 2) @ vec_1
     check_orthogonality(vec_1, vec_2)
     angle, time = np.random.rand(2)
-    new_vec_2, new_angle, new_time = pga.component_2_step_2(points, mean, vec_1, vec_2, angle, time)
+    new_vec_2, new_angle, new_time = pga.component_2_step_2(vec_2, angle, time, points, mean, vec_1)
     new_rotation = make_rotation_2d(new_angle)
     new_mean_2 = (mean + new_time * vec_1) @ new_rotation
     check_unit_norm(new_vec_2)
@@ -91,12 +131,12 @@ def test__bures_wasserstein_pga__step2(dim=3, n_points=40):
 def test__bures_wasserstein_pga__dim2(tol=1e-3, seed=123):
     """Check that implementations in dimension 2 and higher are consistent."""
     np.random.seed(seed)
-    points_spd, _, _ = generate_spd_matrices_on_intersecting_geodesics(dim=2)
+    points_spd, _, _, _ = generate_spd_matrices_on_intersecting_geodesics(dim=2)
     mean_init, vec_init = generate_initialization(points_spd)
     res_pga_2d = BuresWassersteinPGA(dim=2, mean_init=mean_init, vec_1_init=vec_init).fit(points_spd)
-    res_pga_nd = BuresWassersteinPGAND(mean_init=mean_init, vec_1_init=vec_init).fit(points_spd)
-    result = np.array([res_pga_nd['cost_1'], res_pga_nd['cost_2']])
-    expected = np.array([res_pga_2d['cost_1'], res_pga_2d['cost_2']])
+    res_pga_nd = BuresWassersteinPGAND(dim=2, mean_init=mean_init, vec_1_init=vec_init).fit(points_spd)
+    result = np.array([res_pga_nd['costs'][0], res_pga_nd['costs'][1]])
+    expected = np.array([res_pga_2d['costs'][0], res_pga_2d['costs'][1]])
     assert np.all(np.abs((result - expected) / expected) < tol)
 
 
@@ -110,51 +150,76 @@ def test__bures_wasserstein_pga__single_geodesic(dim=2, n_points=41, tol=1e-3, s
     expected = points_spd[n_points // 2]
     assert np.all(np.abs(result - expected) < tol)
 
-    result = pga['component_1']
+    result = pga['components'][0]
     expected = points_spd
     assert np.all(np.abs(result - expected) < tol)
 
-    result = pga['component_2']
+    result = pga['components'][1]
     expected = np.tile(pga['mean_spd'], (n_points, 1, 1))
     assert np.all(np.abs(result - expected) < tol)
 
 
-def test__bures_wasserstein_pga__orthogonal_geodesics(dim=2, n_points=40, tol=1e-3, seed=123):
+def test__bures_wasserstein_pga__two_orthogonal_geodesics(dim=3, n_points=20, tol=1e-3, seed=123):
     """Check that two intersecting geodesics project to themselves."""
-    times_1 = np.linspace(-0.5, 0.5, n_points // 2)
-    times_2 = np.linspace(-0.3, 0.3, n_points // 2)
-    points_spd, mean_spd = generate_spd_matrices_on_orthogonal_geodesics(dim, times_1, times_2, seed)
+    times_1 = np.linspace(-0.5, 0.5, n_points)
+    times_2 = np.linspace(-0.3, 0.3, n_points)
+    points_spd, mean_spd = generate_spd_matrices_on_two_orthogonal_geodesics(dim, times_1, times_2, seed)
     pga = BuresWassersteinPGA(dim).fit(points_spd)
+
+    result = pga['components'][0][:n_points]
+    expected = points_spd[:n_points]
+    assert np.all(np.abs(result - expected) < tol)
+
+    result = pga['components'][0][n_points:]
+    expected = np.tile(mean_spd, (n_points, 1, 1))
+    assert np.all(np.abs(result - expected) < tol)
+
+    result = pga['components'][1][:n_points]
+    expected = np.tile(mean_spd, (n_points, 1, 1))
+    assert np.all(np.abs(result - expected) < tol)
+
+    result = pga['components'][1][n_points:2*n_points]
+    expected = points_spd[n_points:2*n_points]
+    assert np.all(np.abs(result - expected) < tol)
+
+
+def test__bures_wasserstein_pga__orthogonal_geodesics(dim=2, n_points=20, tol=1e-3, seed=123):
+    """Check that multiple orthogonal geodesics project to themselves."""
+    rdim = dim * (dim + 1) // 2
+    decreasing_times = 0.3 + np.arange(rdim)[::-1] / 5
+    times = np.vstack([np.linspace(-t, t, n_points) for t in decreasing_times])
+    points_spd, mean_spd = generate_spd_matrices_on_orthogonal_geodesics(dim, times, seed)
+    pga = BuresWassersteinPGA(dim).fit(points_spd)
+
+    indices = np.arange(rdim * n_points)
+    masks = [(indices < n_points)]
+    for i in range(1, rdim - 1):
+        masks.append((indices >= i * n_points) * (indices < (i+1) * n_points))
+    masks.append((indices >= (rdim - 1) * n_points))
 
     result = pga['mean_spd']
     expected = mean_spd
     assert np.all(np.abs(result - expected) < tol)
 
-    result = pga['component_1'][:n_points // 2]
-    expected = points_spd[:n_points // 2]
-    assert np.all(np.abs(result - expected) < tol)
+    for i in range(rdim):
+        result = pga['components'][i][masks[i]]
+        expected = points_spd[masks[i]]
+        assert np.all(np.abs(result - expected) < tol)
 
-    result = pga['component_1'][n_points // 2:]
-    expected = np.tile(mean_spd, (n_points // 2, 1, 1))
-    assert np.all(np.abs(result - expected) < tol)
-
-    result = pga['component_2'][:n_points // 2]
-    expected = np.tile(mean_spd, (n_points // 2, 1, 1))
-    assert np.all(np.abs(result - expected) < tol)
-
-    result = pga['component_2'][n_points // 2:]
-    expected = points_spd[n_points // 2:]
-    assert np.all(np.abs(result - expected) < tol)
+        result = pga['components'][i][~masks[i]]
+        expected = np.tile(mean_spd, ((rdim - 1) * n_points, 1, 1))
+        #print(np.abs(result - expected))
+        assert np.all(np.abs(result - expected) < tol)
 
 
 def test__bures_wasserstein_pga__orthogonality(dim=2, tol=1e-5, seed=123):
     np.random.seed(seed)
-    points_spd, _, _ = generate_spd_matrices_on_intersecting_geodesics(dim)
+    points_spd, _, _, _ = generate_spd_matrices_on_intersecting_geodesics(dim)
     mean_init, vec_init = generate_initialization(points_spd)
     res_pga = BuresWassersteinPGA(dim=dim, mean_init=mean_init, vec_1_init=vec_init).fit(points_spd)
     spd_space = SPDMatrices(dim)
     spd_space.equip_with_metric(SPDBuresWassersteinMetric)
-    result = spd_space.metric.inner_product(res_pga['vec_1_spd'], res_pga['vec_2_spd'], res_pga['mean_spd'])
+    result = spd_space.metric.inner_product(res_pga['vecs_spd'][0], res_pga['vecs_spd'][1], res_pga['mean_spd'])
     expected = 0.
     assert np.abs(result - expected) < tol
 
@@ -169,52 +234,84 @@ def test__bures_wasserstein_tpca__single_geodesic(dim=2, n_points=41, tol=1e-3, 
     expected = points_spd[n_points // 2]
     assert np.all(np.abs(result - expected) < tol)
 
-    result = tpca['component_1']
+    result = tpca['components'][0]
     expected = points_spd
     assert np.all(np.abs(result - expected) < tol)
 
-    result = tpca['component_2']
+    result = tpca['components'][1]
     expected = np.tile(tpca['mean_spd'], (n_points, 1, 1))
     assert np.all(np.abs(result - expected) < tol)
 
 
-def test__bures_wasserstein_tpca__orthogonal_geodesics(dim=2, n_points=40, tol=1e-3, seed=123):
+def test__bures_wasserstein_tpca__two_orthogonal_geodesics(dim=2, n_points=40, tol=1e-3, seed=123):
     """Check that two intersecting geodesics project to themselves."""
     times_1 = np.linspace(-0.5, 0.5, n_points // 2)
     times_2 = np.linspace(-0.3, 0.3, n_points // 2)
-    points_spd, mean_spd = generate_spd_matrices_on_orthogonal_geodesics(dim, times_1, times_2, seed)
+    points_spd, mean_spd = generate_spd_matrices_on_two_orthogonal_geodesics(dim, times_1, times_2, seed)
     tpca = BuresWassersteinTPCA.fit(points_spd)
 
     result = tpca['mean_spd']
     expected = mean_spd
     assert np.all(np.abs(result - expected) < tol)
 
-    result = tpca['component_1'][:n_points // 2]
+    result = tpca['components'][0][:n_points // 2]
     expected = points_spd[:n_points // 2]
     assert np.all(np.abs(result - expected) < tol)
 
-    result = tpca['component_1'][n_points // 2:]
+    result = tpca['components'][0][n_points // 2:]
     expected = np.tile(mean_spd, (n_points // 2, 1, 1))
     assert np.all(np.abs(result - expected) < tol)
 
-    result = tpca['component_2'][:n_points // 2]
+    result = tpca['components'][1][:n_points // 2]
     expected = np.tile(mean_spd, (n_points // 2, 1, 1))
     assert np.all(np.abs(result - expected) < tol)
 
-    result = tpca['component_2'][n_points // 2:]
+    result = tpca['components'][1][n_points // 2:]
     expected = points_spd[n_points // 2:]
     assert np.all(np.abs(result - expected) < tol)
 
 
+def test__bures_wasserstein_tpca__orthogonal_geodesics(dim=2, n_points=20, tol=1e-3, seed=123):
+    """Check that multiple orthogonal geodesics project to themselves."""
+    rdim = dim * (dim + 1) // 2
+    decreasing_times = 0.3 + np.arange(rdim)[::-1] / 5
+    times = np.vstack([np.linspace(-t, t, n_points) for t in decreasing_times])
+    points_spd, mean_spd = generate_spd_matrices_on_orthogonal_geodesics(dim, times, seed)
+    tpca = BuresWassersteinTPCA.fit(points_spd)
+
+    indices = np.arange(rdim * n_points)
+    masks = [(indices < n_points)]
+    for i in range(1, rdim - 1):
+        masks.append((indices >= i * n_points) * (indices < (i+1) * n_points))
+    masks.append((indices >= (rdim - 1) * n_points))
+
+    result = tpca['mean_spd']
+    expected = mean_spd
+    assert np.all(np.abs(result - expected) < tol)
+
+    for i in range(rdim):
+        result = tpca['components'][i][masks[i]]
+        expected = points_spd[masks[i]]
+        assert np.all(np.abs(result - expected) < tol)
+
+        result = tpca['components'][i][~masks[i]]
+        expected = np.tile(mean_spd, ((rdim - 1) * n_points, 1, 1))
+        assert np.all(np.abs(result - expected) < tol)
+
+
+test__bures_wasserstein_pga__init_dim2()
 test__bures_wasserstein_pga__step1(dim=3)
 test__bures_wasserstein_pga__step2(dim=3)
 test__bures_wasserstein_pga__step2_dim2()
 test__bures_wasserstein_pga__dim2()
 test__bures_wasserstein_pga__single_geodesic(dim=2)
 test__bures_wasserstein_pga__single_geodesic(dim=3)
+test__bures_wasserstein_pga__two_orthogonal_geodesics(dim=2)
+test__bures_wasserstein_pga__two_orthogonal_geodesics(dim=3)
 test__bures_wasserstein_pga__orthogonal_geodesics(dim=2)
-test__bures_wasserstein_pga__orthogonal_geodesics(dim=3)
+#test__bures_wasserstein_pga__orthogonal_geodesics(dim=3)
 test__bures_wasserstein_pga__orthogonality(dim=2)
 test__bures_wasserstein_pga__orthogonality(dim=3)
 test__bures_wasserstein_tpca__single_geodesic(dim=3)
-test__bures_wasserstein_tpca__orthogonal_geodesics(dim=3)
+test__bures_wasserstein_tpca__two_orthogonal_geodesics(dim=2)
+test__bures_wasserstein_tpca__orthogonal_geodesics(dim=2)
---
+++ tests/test_tools.py
@@ -0,0 +1,88 @@
+from tools.compute import *
+from tools.generate import generate_spd_matrices_on_intersecting_geodesics
+from geomstats.geometry.special_orthogonal import SpecialOrthogonal
+
+
+def test_make_rotation_2d_vectorization(tol=1e-5):
+    angles = np.random.rand(5)
+    result = make_rotation_2d(angles)
+    expected = np.stack([make_rotation_2d(angle) for angle in angles])
+    assert np.all(np.abs(result - expected) < tol)
+
+
+def test_projection_vectorization(tol=1e-5):
+    mats = np.random.rand(5, 2, 2)
+    result = project(mats)
+    expected = np.stack([project(mat) for mat in mats])
+    assert np.all(np.abs(result - expected) < tol)
+
+
+def test_align_vectorization(tol=1e-5):
+    ref_mat = np.random.rand(2, 2)
+    mats = np.random.rand(5, 2, 2)
+    covs = project(mats)
+    result = align(covs, ref_mat)
+    expected = np.stack([align(cov, ref_mat) for cov in covs])
+    assert np.all(np.abs(result - expected) < tol)
+
+
+def test_align(tol=1e-5):
+    spd_space = SPDMatrices(2)
+    spd = spd_space.random_point()
+    ref_mat = sqrtm(spd)
+    spds = spd_space.random_point(5)
+    mats = align(spds, ref_mat)
+    assert np.all(np.abs(project(mats) - spds) < tol)
+
+
+def test_bures_wasserstein_sectional_curvature(tol=1e-5):
+    np.random.seed(1)
+    dim = 2
+    rot_space = SpecialOrthogonal(dim)
+    spd_space = SPDMatrices(dim)
+    spd_space.equip_with_metric(SPDBuresWassersteinMetric)
+    metric = spd_space.metric
+    d = np.random.rand(dim)
+    P = rot_space.random_point()
+    base_point = P @ np.diag(d) @ P.T
+    vec_1 = spd_space.random_tangent_vec(base_point)
+    vec_2 = spd_space.random_tangent_vec(base_point)
+    result = bures_wasserstein_sectional_curvature(vec_1, vec_2, base_point)
+
+    vec_1 /= metric.norm(vec_1, base_point)
+    vec_2 = vec_2 - metric.inner_product(vec_1, vec_2, base_point) * vec_1
+    vec_2 /= metric.norm(vec_2, base_point)
+    expected = bures_wasserstein_sectional_curvature(vec_1, vec_2, base_point)
+    assert np.abs(result - expected) < tol
+
+    vec_1_prime = P.T @ vec_1 @ P
+    vec_2_prime = P.T @ vec_2 @ P
+    vec_1_0_prime = np.zeros((dim, dim))
+    vec_2_0_prime = np.zeros((dim, dim))
+    coef = np.zeros((dim, dim))
+    for i in range(dim):
+        for j in range(dim):
+            vec_1_0_prime[i, j] = vec_1_prime[i, j] / (d[i] + d[j])
+            vec_2_0_prime[i, j] = vec_2_prime[i, j] / (d[i] + d[j])
+            coef[i, j] = d[i] * d[j] / (d[i] + d[j])
+    bracket = vec_1_0_prime @ vec_2_0_prime - vec_2_0_prime @ vec_1_0_prime
+    expected = 3/2 * np.sum(coef * bracket ** 2)
+    assert np.abs(result - expected) < tol
+
+
+def test_bures_wasserstein_ricci_curvature_vectorization(tol=1e-5, n_points=10):
+    dim = 2
+    spd_space = SPDMatrices(dim)
+    base_points = spd_space.random_point(n_points)
+    vecs = spd_space.random_tangent_vec(base_points)
+    result = bures_wasserstein_ricci_curvature(vecs, base_points)
+    expected = np.array([bures_wasserstein_ricci_curvature(vc, bp) for (vc, bp) in zip(vecs, base_points)])
+    assert np.all(np.abs(result - expected) < tol)
+
+
+test_make_rotation_2d_vectorization()
+test_projection_vectorization()
+test_align_vectorization()
+test_align()
+test_bures_wasserstein_sectional_curvature()
+test_bures_wasserstein_ricci_curvature_vectorization()
